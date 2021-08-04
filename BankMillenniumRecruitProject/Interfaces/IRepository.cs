b713dce96f59397389e0aacfd53374835261707a
f0urth <f0urth.kamil@gmial.com>
2021-08-04 19:04:59 +0200

    Add project files.

---
+++ BankMillenniumRecruitProject/Interfaces/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankMillenniumRecruitProject.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        public void Add(T entity);
+
+        public void Remove(T entity);
+
+        public void Update(T entity);
+
+        public T Get(long id);
+
+        public IQueryable<T> GetAll();
+    }
+}
